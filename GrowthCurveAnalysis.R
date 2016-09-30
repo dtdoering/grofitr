0a7aa3fee5061b0b2f2dd049e39751ae169eb128
Drew Doering <dtdoering@wisc.edu>
2016-09-30 15:17:41 -0500

    Switch to '<-' for assignment

--- GrowthCurveAnalysis.R
+++ GrowthCurveAnalysis.R
@@ -27,8 +27,8 @@ for(i in seq_along(ExperimentNames)){
 truncTime = 40 # hours
 
 # Set output destination of GroFit_df and PDF of plots
-df_dest = "/Users/dtdoering/1_Research/Lab/DATA/phenotyping/plate\ reader/Output/R5D1_GroFit_df.csv"
-plot_dest = "/Users/dtdoering/1_Research/Lab/DATA/phenotyping/plate\ reader/Output/"
+df_dest <- "/Users/dtdoering/1_Research/Lab/DATA/phenotyping/plate\ reader/Output/R5D1_GroFit_df.csv"
+plot_dest <- "/Users/dtdoering/1_Research/Lab/DATA/phenotyping/plate\ reader/Output/"
 
 setwd(path)
 
@@ -69,39 +69,39 @@ for(i in seq_along(DataFiles)){
 
 # Creates a list of time points used in each experiment
 # This list is based on the the numeric values not the long names (e.g. 1hr - 2hr)
-timePoints_list = list()
+timePoints_list <- list()
 for(i in PlateNames){
-  timePoints_list[[i]] = list()
-  timePoints = data.frame(get(i)[1,4:ncol(get(i))])
-  timePoints = apply(timePoints,2,function(x) gsub('\\.*(\\d*) h ?(\\d*).*', '\\1.0\\2',x))
+  timePoints_list[[i]] <- list()
+  timePoints <- data.frame(get(i)[1,4:ncol(get(i))])
+  timePoints <- apply(timePoints,2,function(x) gsub('\\.*(\\d*) h ?(\\d*).*', '\\1.0\\2',x))
   for(j in seq_along(timePoints)){
     timePoints[j] <- as.numeric(unlist(strsplit(timePoints[j], '\\.'))[1]) + as.numeric(unlist(strsplit(timePoints[j], '\\.'))[2])/60
   }
-  timePoints_list[[i]]$timePoints = as.numeric(timePoints[as.numeric(timePoints) <= truncTime & is.na(timePoints) == F])
+  timePoints_list[[i]]$timePoints <- as.numeric(timePoints[as.numeric(timePoints) <= truncTime & is.na(timePoints) == F])
 }
 
 # Create a time matrix for each plate
-timeMatrix_list = list()
+timeMatrix_list <- list()
 for(i in names(timePoints_list)){
-  timeMatrix_list[[i]] = list()
-  TIMES = rbind(timePoints_list[[i]]$timePoints)
-  timePoints_m = data.frame(TIMES)
+  timeMatrix_list[[i]] <- list()
+  TIMES <- rbind(timePoints_list[[i]]$timePoints)
+  timePoints_m <- data.frame(TIMES)
   for(j in 2:nrow(get(i))){
-    timePoints_m[j,] = timePoints_m
+    timePoints_m[j,] <- timePoints_m
   }
-  timeMatrix_list[[i]] = as.matrix(timePoints_m)
+  timeMatrix_list[[i]] <- as.matrix(timePoints_m)
 }
 
 # Modify the input raw data ===================================================
 # Remove the timepoint row in the data table
 for(i in seq_along(PlateNames)){
-  filename = PlateNames[[i]]
+  filename <- PlateNames[[i]]
   assign(filename, get(filename)[-1,])
 }
 
 # Merge Plate Info with growth data based on well
 for(i in seq_along(PlateNames)){
-  filename = PlateNames[i]
+  filename <- PlateNames[i]
   assign(filename,
     merge(
       PlateInfos[[i]][ , c("Well.Row", "Well.Col", "Homolog", "feConc", "cuConc")],
@@ -112,41 +112,41 @@ for(i in seq_along(PlateNames)){
 }
 
 # Drop columns no longer needed from the plate data objects
-DropColumns = c("Well.Row",
+DropColumns <- c("Well.Row",
                 "Well.Col",
                 "Content")
 for(i in seq_along(PlateNames)){
-  filename = PlateNames[[i]]
+  filename <- PlateNames[[i]]
   assign(filename, get(filename)[,!(colnames(get(filename)) %in% DropColumns)])
 }
 
 # Run GroFit for all plates ==================================================
 if(!exists("GroFitResults")){
   cat(noquote("GroFitResults not found - creating now"), '\n')
-  GroFitResults = list()
+  GroFitResults <- list()
 } else {
   cat(noquote("GroFitResults exists - Results will be added"), '\n')
 }
 cat(noquote("Running GroFit on all plates..."), '\n')
 for(i in PlateNames){
-  GroFitResults[[i]] = list()
+  GroFitResults[[i]] <- list()
   for(j in 1:nrow(get(i))){
-    GroFitResults[[i]][[j]] = list()
-    GroFitResults[[i]][[j]]$Plate = i
+    GroFitResults[[i]][[j]] <- list()
+    GroFitResults[[i]][[j]]$Plate <- i
 
     # Use the 3 columns taken from Plate Info to name corresponding data in GroFitResults
-    GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[1]]] = get(i)[j,1]
+    GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[1]]] <- get(i)[j,1]
                                             # Should  ^ this be i?
-    GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[2]]] = get(i)[j,2]
+    GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[2]]] <- get(i)[j,2]
                                             # Should  ^ this be i?
-    GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[3]]] = get(i)[j,3]
+    GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[3]]] <- get(i)[j,3]
                                             # Should  ^ this be i?
 
-    TimeData = t(data.frame(timeMatrix_list[[i]][j,]))
-    TimeData = TimeData[, 1:ncol(TimeData), drop = F]
+    TimeData <- t(data.frame(timeMatrix_list[[i]][j,]))
+    TimeData <- TimeData[, 1:ncol(TimeData), drop = F]
 
-    GrowthData = t(data.frame(as.numeric(get(i)[j,])))
-    GrowthData = GrowthData[, 1:(ncol(TimeData)+3), drop = F]
+    GrowthData <- t(data.frame(as.numeric(get(i)[j,])))
+    GrowthData <- GrowthData[, 1:(ncol(TimeData)+3), drop = F]
 
     GroFitResults[[i]][[j]]$GroFitResults = grofit(TimeData, GrowthData,
       control = grofit.control(
@@ -155,7 +155,7 @@ for(i in PlateNames){
         interactive = FALSE,
         nboot.gc = 100,
         # smooth.gc  = 5
-        )
+      )
     )
   }
   cat(noquote(paste("  Added results of plate ", i, ".", sep = "")), '\n')
@@ -164,7 +164,7 @@ cat(noquote("  Results complete!"), '\n')
 
 # Creates a data table of lag, growth rate, and saturation ====================
 cat(noquote("Summarizing results..."), '\n')
-GroFit_df = data.frame(
+GroFit_df <- data.frame(
   Plate = character(),
   A = character(),
   B = character(),
@@ -177,17 +177,17 @@ GroFit_df = data.frame(
 colnames(GroFit_df)[2:4] <- c(colnames(get(PlateNames[[1]]))[1], # "Homolog"
                               colnames(get(PlateNames[[1]]))[2], # "feConc"
                               colnames(get(PlateNames[[1]]))[3]) # "cuConc"
-k = 1
+k <- 1
 for(i in seq_along(GroFitResults)){
   for(j in seq_along(GroFitResults[[i]])){
-    GroFit_df[k,1] = GroFitResults[[i]][[j]]$Plate
-    GroFit_df[k,2] = GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[1]]]
-    GroFit_df[k,3] = GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[2]]]
-    GroFit_df[k,4] = GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[3]]]
-    GroFit_df[k,5] = GroFitResults[[i]][[j]]$GroFitResults[["gcFit"]][["gcTable"]][["lambda.model"]]
-    GroFit_df[k,6] = GroFitResults[[i]][[j]]$GroFitResults[["gcFit"]][["gcTable"]][["mu.model"]]
-    GroFit_df[k,7] = GroFitResults[[i]][[j]]$GroFitResults[["gcFit"]][["gcTable"]][["A.model"]]
-    k = 1 + k
+    GroFit_df[k,1] <- GroFitResults[[i]][[j]]$Plate
+    GroFit_df[k,2] <- GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[1]]]
+    GroFit_df[k,3] <- GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[2]]]
+    GroFit_df[k,4] <- GroFitResults[[i]][[j]][[colnames(get(PlateNames[[1]]))[3]]]
+    GroFit_df[k,5] <- GroFitResults[[i]][[j]]$GroFitResults[["gcFit"]][["gcTable"]][["lambda.model"]]
+    GroFit_df[k,6] <- GroFitResults[[i]][[j]]$GroFitResults[["gcFit"]][["gcTable"]][["mu.model"]]
+    GroFit_df[k,7] <- GroFitResults[[i]][[j]]$GroFitResults[["gcFit"]][["gcTable"]][["A.model"]]
+    k <- 1 + k
   }
   assign(paste(names(GroFitResults)[[i]], "_GroFit_df", sep = ""), GroFit_df)
 }
@@ -214,112 +214,112 @@ makeplots <- function(platename){
   )
 
   for(well in seq_along(GroFitResults[[platename]])){
-    times = GroFitResults[[platename]][[well]]$GroFitResults$gcFit$gcFittedModels[[1]]$raw.time
-    od = GroFitResults[[platename]][[well]]$GroFitResults$gcFit$gcFittedModels[[1]]$raw.data
-    temp_df = data.frame(cbind(times, od))
+    times <- GroFitResults[[platename]][[well]]$GroFitResults$gcFit$gcFittedModels[[1]]$raw.time
+    od <- GroFitResults[[platename]][[well]]$GroFitResults$gcFit$gcFittedModels[[1]]$raw.data
+    temp_df <- data.frame(cbind(times, od))
     colnames(temp_df) = c("Time", "Absorbance")
 
-    Aobs = summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$A.model
-    A.upCI =  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.A.model.up
-    A.loCI =  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.A.model.lo
+    Aobs <- summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$A.model
+    A.upCI <-  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.A.model.up
+    A.loCI <-  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.A.model.lo
 
-    muobs =  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$mu.model
-    mu.upCI =  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.mu.model.up
-    mu.loCI =  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.mu.model.lo
+    muobs <-  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$mu.model
+    mu.upCI <-  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.mu.model.up
+    mu.loCI <-  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.mu.model.lo
 
-    lambdaobs =  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$lambda.model
-    lambda.upCI =  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.lambda.model.up
-    lambda.loCI =  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.lambda.model.lo
+    lambdaobs <-  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$lambda.model
+    lambda.upCI <-  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.lambda.model.up
+    lambda.loCI <-  summary(GroFitResults[[platename]][[well]]$GroFitResults$gcFit)$ci95.lambda.model.lo
 
     if(is.null(lambdaobs)){
-      lambdaobs = 0
+      lambdaobs <- 0
     }
 
     # ggplot statements =======================================================
     # growth data points ------------------------------------------------------
-    curve = ggplot(temp_df, aes(x = Time, y = Absorbance))
-    curve = curve + geom_point(pch = 19)
+    curve <- ggplot(temp_df, aes(x = Time, y = Absorbance))
+    curve <- curve + geom_point(pch = 19)
 
     # saturation point with confidence intervals ------------------------------
     if(!is.null(Aobs) & !is.na(Aobs) & !is.nan(A.upCI) & !is.nan(A.loCI)){
-      curve = curve +
+      curve <- curve +
       geom_hline(
         yintercept = Aobs,
         color = "blue"
-        )
+      )
     }
     if(!is.nan(A.upCI) & !is.na(A.upCI)){
-      curve = curve +
+      curve <- curve +
       geom_hline(
         yintercept = A.upCI,
         col = "cyan",
         lty = 2
-        )
+      )
     }
     if(!is.nan(A.loCI) & !is.na(A.loCI)){
-      curve = curve +
+      curve <- curve +
       geom_hline(
         yintercept = A.loCI,
         col = "cyan",
         lty = 2
-        )
+      )
     }
 
     # lag time with confidence intervals --------------------------------------
     if(!is.null(lambdaobs) & !is.na(lambdaobs) & !is.nan(lambda.upCI) & !is.nan(lambda.loCI) & 0 < lambdaobs & lambdaobs < truncTime){
-     curve = curve +
-     geom_vline(
-       xintercept = lambdaobs,
-       color = "green4"
-       )
+      curve <- curve +
+      geom_vline(
+        xintercept = lambdaobs,
+        color = "green4"
+      )
     }
     if(!is.nan(lambda.upCI) & !is.na(lambda.upCI) & 0 < lambda.upCI & lambda.upCI < truncTime){
-      curve = curve +
+      curve <- curve +
       geom_vline(
         xintercept = lambda.upCI,
         col = "green",
         lty = 2
-        )
+      )
     }
     if(!is.nan(lambda.loCI) & !is.na(lambda.loCI) & 0 < lambda.loCI & lambda.loCI < truncTime){
-      curve = curve +
+      curve <- curve +
       geom_vline(
         xintercept = lambda.loCI,
         col = "green",
         lty = 2
-        )
+      )
     }
 
     # max growth rate with confidence intervals -------------------------------
     if(!is.null(muobs) & !is.na(muobs) & !is.nan(mu.upCI) & !is.nan(mu.loCI) & 0 < lambdaobs & lambdaobs < truncTime){
-      curve = curve +
+      curve <- curve +
       geom_abline(
         intercept = -(muobs*lambdaobs),
         slope = muobs,
         col = "red"
-        )
+      )
     }
     if(!is.nan(mu.upCI) & !is.na(mu.upCI)){
-      curve = curve +
+      curve <- curve +
       geom_abline(
         intercept = -(muobs*lambdaobs),
         slope = mu.upCI,
         col = "orange",
         lty = 2
-        )
+      )
     }
     if(!is.nan(mu.loCI) & !is.na(mu.loCI) & 0 < lambdaobs & lambdaobs < truncTime){
-      curve = curve +
+      curve <- curve +
       geom_abline(
         intercept = -(muobs*lambdaobs),
         slope = mu.loCI,
         col = "orange",
         lty = 2
-        )
+      )
     }
 
     # format plots - axes, max/min, gridlines, etc. ---------------------------
-    curve = curve +
+    curve <- curve +
     ylim(0, 2) +
     scale_x_continuous(breaks = seq(0,150,10)) +
     theme(
@@ -328,8 +328,7 @@ makeplots <- function(platename){
       panel.background = element_blank(),
       axis.line.x = element_line(colour = "black"),
       axis.line.y = element_line(color = "black")
-      )
-    curve = curve +
+    ) +
     xlab("Time (h)") +
     ylab("Absorbance") +
 
@@ -341,7 +340,7 @@ makeplots <- function(platename){
       GroFitResults[[platename]][[well]][[colnames(get(PlateNames[[1]]))[3]]],
       ")",
       sep = "")
-      )
+    )
     print(curve)
   }
   dev.off()
@@ -353,7 +352,7 @@ for(i in names(GroFitResults)){
   cat(noquote('Done.'), '\n')
 }
 
-# Cleanup - remove intermediate variables that aren't part of final output ==
+# Cleanup - remove intermediate variables that aren't part of final output ====
 rm(list = c("A.loCI", "A.upCI", "Aobs", "curve", "df_dest", "DropColumns", "Experiment_list", "ExperimentName", "ExperimentPlates", "filename", "GroFit_df", "GrowthData", "i", "j", "k", "lambda.loCI", "lambda.upCI", "lambdaobs", "mu.loCI", "mu.upCI", "muobs", "od", "PlateName_list", "PlateNames", "plot_dest", "temp_df", "TimeData", "timeMatrix_list", "timePoints", "timePoints_list", "timePoints_m", "TIMES", "times", "truncTime", "usage"))
 
 cat(noquote("GroFit script complete."), '\n')
