78ac9415186c6e6457a066b93fc9da8461db7722
Drew Doering <dtdoering@wisc.edu>
2016-11-09 12:50:22 -0600

    bypass creation of TIMES temporary variable

--- GrowthCurveAnalysis.R
+++ GrowthCurveAnalysis.R
@@ -72,8 +72,7 @@ for (i in PlateNames) {
 timeMatrix_list <- list()
 for (i in names(timePoints_list)) {
   timeMatrix_list[[i]] <- list()
-  TIMES <- rbind(timePoints_list[[i]]$timePoints)
-  timePoints_m <- data.frame(TIMES)
+  timePoints_m <- data.frame(rbind(timePoints_list[[i]]$timePoints))
   for (j in 2:nrow(get(i))) {
     timePoints_m[j,] <- timePoints_m
   }
