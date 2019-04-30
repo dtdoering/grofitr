9e6e7c076e301d3ad0d3913b569ed90a50e9d07c
Drew T. Doering <dtdoering@wisc.edu>
2019-04-30 15:11:20 -0500

    Misc fixes

--- R/load_BMG.R
+++ R/load_BMG.R
@@ -16,22 +16,21 @@
 
 # Add option to extract barcode? Would store in "plate" column.
 
-load_BMG <- function(file, get.barcode = FALSE) {
+load_BMG <- function(file, time.limits = c(0, Inf), get.barcode = FALSE) {
   x <- file %>%
     readLines() %>%
     grepl("Well Row", .) %>%
     which() %>%
     `-`(1) %>%
     read_csv(file, skip = .)
-  y <- x %>% rename_at(vars(-c(1:3)),
+  x <- x %>% rename_at(vars(-c(1:3)),
                        funs(x %>% slice(1) %>% select(-c(1:3)))) %>%
     dplyr::slice(-1) %>%
     tidyr::gather(time, OD, -`Well Row`, -`Well Col`, -Content) %>%
-    dplyr::mutate(.data = ., time = time_to_dbl(time), OD = as.numeric(OD))
   if (get.barcode == TRUE) {
-    y <-  dplyr::mutate(.data = y, plate = get_barcode_BMG(file))
+    x <-  dplyr::mutate(.data = x, plate = get_barcode_BMG(file))
   } else {
-    y <-  dplyr::mutate(.data = y, plate = basename(file) %>%
+    x <-  dplyr::mutate(.data = x, plate = basename(file) %>%
                           strsplit("\\.") %>% `[[`(1) %>% `[`(1))
   }
 }
